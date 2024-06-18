e266d89ec45d46e4ab51f2a37f7bc49f2d04aa2f
Michael Vega <micha3lvega@gmail.com>
2024-06-17 23:42:19 -0500

    Crear tabla de usuarios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-app';
+  title = 'Hola desde el componente';
+  users = ['user1','user2','user3','user4','user5']
+  // users = [];
+  // users? : string[]; //  Se le dice al compilador que ya se que no esta inicializada
+  visible : boolean = true;
+  // users!: string[]; // Se le dice al compilador que esta variable es opcional
+
+
+  setVisible() : void {
+    this.visible = !this.visible;
+  }
+
 }
